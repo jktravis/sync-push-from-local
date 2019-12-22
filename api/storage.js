bdbcd14d3d9930987e318354ca105b6ba5299624
jktravis <joshua.travis@gmail.com>
2019-12-21 20:23:23 -0500

    seems to be working

---
+++ api/storage.js
@@ -0,0 +1,20 @@
+import localForage from 'localforage';
+import * as R from 'ramda';
+
+const syncItem = R.curry((mapper, data) => {
+  return localForage.getItem(data.id).then(item => {
+    if (item) {
+      return mapper(item);
+    }
+    return data;
+  });
+});
+
+const saveItem = (item) => {
+  return localForage.setItem(item.id, item).then(() => item);
+};
+
+export {
+  syncItem,
+  saveItem,
+};
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from 'react';
 import {FixedSizeList as List} from 'react-window';
 import io from 'socket.io-client';
 import * as R from 'ramda';
+import {syncItem, saveItem} from '../api/storage';
 
 const Row = ({id, message, onClick, toggled}) => (
   <li>
-    <button onClick={() => onClick(id)}>{toggled? 'on' : 'off'}</button> {message} {id}
+    <button onClick={() => onClick(id)}>{String(toggled)}</button> {message}{' '}
+    {id}
   </li>
 );
 
@@ -14,29 +16,55 @@ const remapData = R.compose(
   R.map(R.assoc('toggled', false)),
 );
 
+const merge = R.curry((d, item) => {
+  return R.assoc('toggled', item.toggled, d);
+});
+
 function Hello() {
   const [data, setData] = useState([]);
 
-  const toggleRow = id => setData(R.over(R.lensPath([id, 'toggled']), R.not, data));
+  const toggleRow = id => {
+    const lens = R.lensPath([id, 'toggled']);
+    const newData = R.over(lens, R.not, data);
+    saveItem(R.compose(R.pick(['id', 'toggled']), R.prop(id))(newData))
+      .then()
+      .catch();
+    setData(newData);
+  };
 
   useEffect(() => {
     const socket = io();
-    socket.on('now', R.compose(setData, remapData, R.prop('data')));
+    socket.on('now', resp => {
+      const d = R.compose(remapData, R.prop('data'))(resp);
+      Promise.all(
+        R.compose(
+          R.map(d => syncItem(merge(d), d)),
+          R.values,
+        )(d),
+      ).then(
+        R.compose(
+          setData,
+          R.indexBy(R.prop('id')),
+        ),
+      );
+    });
     return socket.close.bind(socket);
-  });
+  }, []);
 
   const values = R.values(data);
 
   if (R.length(values)) {
     return (
-      <ul style={{listStyle: 'none'}}>
-        {R.map(
-          row => (
-            <Row key={row.id} {...row} onClick={toggleRow} />
-          ),
-          values,
-        )}{' '}
-      </ul>
+      <>
+        <ul style={{listStyle: 'none'}}>
+          {R.map(
+            row => (
+              <Row key={row.id} {...row} onClick={toggleRow} />
+            ),
+            values,
+          )}{' '}
+        </ul>
+      </>
     );
   }
   return <div>No data!</div>;
